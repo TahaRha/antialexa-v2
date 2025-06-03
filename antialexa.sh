dc652b441f9ed294dea162f1449963d3d25ac3f4
cosmo <74021800+TahaRha@users.noreply.github.com>
2025-06-03 15:34:07 -0400

    Improve portability and token info

--- antialexa.sh
+++ antialexa.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
-log_file=$HOME/Desktop/projects/antialexa-v2/antialexa.log
-env_log_file=$HOME/Desktop/projects/antialexa-v2/antialexa-env.log
-debug_log_file=$HOME/Desktop/projects/antialexa-v2/antialexa-debug.log
+script_dir="$(cd "$(dirname "$0")" && pwd)"
+log_file="$script_dir/antialexa.log"
+env_log_file="$script_dir/antialexa-env.log"
+debug_log_file="$script_dir/antialexa-debug.log"
+alexa_ctl="$script_dir/alexa_remote_control.sh"
 
 # Introduce logs
-echo "Bluetooth device connected" >> $log_file
-env >> $env_log_file
+echo "Bluetooth device connected" >> "$log_file"
+env >> "$env_log_file"
 
 # Muting Alexa
-echo "Muting Alexa" >> $log_file
-/home/cosmo/Desktop/projects/antialexa-v2/alexa_remote_control.sh -e vol:0 >> $debug_log_file 2>&1
+echo "Muting Alexa" >> "$log_file"
+"$alexa_ctl" -e vol:0 >> "$debug_log_file" 2>&1
 
 # Log of sleeping
-echo "Sleeping for 5 seconds" >> $log_file
+echo "Sleeping for 5 seconds" >> "$log_file"
 sleep 5
 
 # Set volume back to 100
-/home/cosmo/Desktop/projects/antialexa-v2/alexa_remote_control.sh -e vol:100 >> $debug_log_file 2>&1
-echo "Unmuting Alexa" >> $log_file
+"$alexa_ctl" -e vol:100 >> "$debug_log_file" 2>&1
+echo "Unmuting Alexa" >> "$log_file"
